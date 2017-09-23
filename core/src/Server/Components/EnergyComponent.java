94b1975e208a8a40939d3dcd03bceaf2fbf55c58
Samuel Bond <bonds3@go.stockton.edu>
2017-09-23 17:37:41 -0400

    ~Changed starting energy

--- core/src/Server/Components/EnergyComponent.java
+++ core/src/Server/Components/EnergyComponent.java
@@ -5,7 +5,7 @@ import com.badlogic.ashley.core.Component;
 public class EnergyComponent implements Component{
 
 	public final int MAX_ENERGY = 100;
-	public final int ENERGY_PER_LETTER = 5;
+	public final int ENERGY_PER_LETTER = 2;
 	public int energy = 50;
 	
 }
